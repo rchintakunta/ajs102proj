ba7cfcf861786204c201f437ecebc77b75343df9
RNC <nagendracr@gmail.com>
2015-11-13 21:48:46 -0500

    ajs102_project

---
+++ public/js/controllers/controllers.js
@@ -0,0 +1,96 @@
+'use strict';
+angular.module('app.controllers', []).controller('StockCtrl', function ($scope, $http) {
+
+    var self = this;
+    self.symbol = "";
+    self.startDate = "";
+    self.endDate = "";
+    self.result = {};
+    self.tableDataHistorical = [];
+
+    self.getData = function () {
+
+        var url = "http://query.yahooapis.com/v1/public/yql";
+        var symbol = self.symbol;
+        var data = encodeURIComponent("select * from yahoo.finance.quotes where symbol in ('" + self.symbol + "')");
+
+        /*
+         Build the string to use with with $http get to retrieve JSON data from Yahoo Finance API
+         Required format is:
+         http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20('aapl')&format=json&diagnostics=true&env=http://datatables.org/alltables.env
+         */
+        var str1 = url.concat("?q=", data);
+        str1 = str1.concat("&format=json&diagnostics=true&env=http://datatables.org/alltables.env");
+
+        $http.get(str1)
+
+            .success(function (data, status, headers, config) {
+                console.log("success data, status="+ JSON.stringify(data) + status);
+                if (data.query.results == null) {
+                    console.log("No Valid Results could be Returned!!")
+                }
+                else {
+                    self.result.Name = data.query.results.quote.Name;
+                    self.result.Exchange = data.query.results.quote.StockExchange;
+                    self.result.MarketCap = data.query.results.quote.MarketCapitalization;
+                    self.result.LastPrice = data.query.results.quote.LastTradePriceOnly;
+                    self.result.PercentChange = data.query.results.quote.PercentChange;
+                    self.result.YearRange = data.query.results.quote.YearRange;
+                }
+            })
+
+            .error(function (data, status, headers, config) {
+                var err = status + ", " + data;
+                self.result = "Request failed: " + err;
+            });
+
+
+    }
+
+    self.getHistoricalData = function () {
+
+        self.getData(self.symbol);
+
+        var url = "http://query.yahooapis.com/v1/public/yql";
+        var symbol = self.symbol;
+        self.startDate = $("#startDate").val();
+        self.endDate = $("#endDate").val();
+        var dataHistorical = encodeURIComponent("select * from yahoo.finance.historicaldata where symbol in ('" + self.symbol + "') " +
+            " and startDate = '" + self.startDate + "' " +
+            " and endDate = '" + self.endDate + "'");
+
+
+        var str2 = url.concat("?q=", dataHistorical);
+        str2 = str2.concat("&format=json&diagnostics=true&env=store://datatables.org/alltableswithkeys&callback=");
+
+        console.log('startDate: ', self.startDate, ' endDate: ', self.endDate);
+
+        $http.get(str2)
+            .success(function (data, status, headers, config) {
+                console.log("success data historical, status=" + JSON.stringify(data) + status);
+                if (data.query.results == null) {
+                    console.log("No Valid Historical Results could be Returned!!")
+                }
+                else {
+                    self.tableDataHistorical = data.query.results.quote;
+                }
+            })
+
+            .error(function (data, status, headers, config) {
+                var err = status + ", " + data;
+                self.result = "Historical Request failed: " + err;
+            });
+    }
+
+
+    self.clearData = function () {
+        self.symbol = "";
+        self.result = {};
+        self.tableDataHistorical = [];
+        self.historicalDisplay = "";
+        self.startDate = "";
+        self.endDate = "";
+    }
+
+})
+
